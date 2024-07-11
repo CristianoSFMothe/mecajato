ac31b7d2969be52aa30e8b5ee573fafa42df58a7
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2024-07-11 18:01:28 -0300

    🐛 bugfix: invalid url

--- clientes/views.py
+++ clientes/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def clientes(request):
-  return HttpResponse('Estou em Clientes')
+    return render(request, 'clientes.html')
--- mecajado/settings.py
+++ mecajado/settings.py
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'mecajado.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
