e77a3f521841f262c650afa96029c6e11caa8d7e
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2024-07-11 22:38:40 -0300

    ⭐ feat: initial function add car

--- clientes/views.py
+++ clientes/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def clientes(request):
-    return render(request, 'clientes.html')
+    if request.method == "GET":
+        return render(request, 'clientes.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        email = request.POST.get('email')
+        cpf = request.POST.get('cpf')
+        
+        carros = request.POST.getlist('carro')
+        placas = request.POST.getlist('placa')
+        anos = request.POST.getlist('ano')
+        
+        return HttpResponse('Teste')
