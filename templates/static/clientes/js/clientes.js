dc04a1c75138f82d38d9149e0e2c468613588083
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2024-07-12 10:07:10 -0300

    🆙 feat: showing and hiding the form

--- templates/static/clientes/js/clientes.js
+++ templates/static/clientes/js/clientes.js
@@ -11,3 +11,17 @@ function add_carro(){
 
   container.innerHTML += html
 }
+
+function exibir_form(tipo) {
+
+  add_cliente = document.getElementById('adicionar-cliente')
+  att_cliente = document.getElementById('att_cliente')
+
+  if (tipo == '1') {
+    att_cliente.style.display = 'none'
+    add_cliente.style.display = 'block'
+  } else if(tipo == '2') {
+    add_cliente.style.display = 'none'
+    att_cliente.style.display = 'block'
+  }
+}
